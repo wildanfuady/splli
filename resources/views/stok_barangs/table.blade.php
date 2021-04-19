a54851b4b209186c2fe95990f482af55eb1c5504
wildanfuady <wildanfuady@gmail.com>
2021-04-19 07:11:45 +0700

    perbaikan index barang dan stok barang

--- resources/views/stok_barangs/table.blade.php
+++ resources/views/stok_barangs/table.blade.php
@@ -2,7 +2,7 @@
     @include('layouts.datatables_css')
 @endsection
 
-{!! $dataTable->table(['width' => '100%', 'class' => 'table table-striped table-bordered']) !!}
+{!! $dataTable->table(['width' => '100%', 'class' => 'table table-striped table-bordered table-responsive']) !!}
 
 @push('third_party_scripts')
     @include('layouts.datatables_js')
